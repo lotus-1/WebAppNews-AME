b0516caef86b1a22e2ee34fa9c397b2e7e77d82f
maha276 <mahaforo276@gmail.com>
2019-04-25 00:04:22 +0300

    add some testing

---
+++ test.js
@@ -0,0 +1,34 @@
+const test = require('tape');
+const supertest = require('supertest');
+const router = require('./src/router');
+
+test('Initialise', (t) => {
+  let num = 2
+  t.equal(num, 2, 'Should return 2');
+  t.end();
+});
+
+
+test("handlerHome route returns a status code of 200", t => {
+  supertest(router)
+    .get("/")
+    .expect(200)
+    .expect("Content-Type", /html/)
+    .end((err, res) => {
+      t.error(err);
+      t.equal(res.statusCode, 200, "Should return 200");
+      t.end();
+    });
+});
+
+test("handlerData route returns a status code of 200", t => {
+  supertest(router)
+    .get('/search')
+    .expect(200)
+    .expect("Content-Type", /html/)
+    .end((err, res) => {
+      t.error(err);
+      t.equal(res.statusCode, 200, "Should return 200");
+      t.end();
+    });
+});
