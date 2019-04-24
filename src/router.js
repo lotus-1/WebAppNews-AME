e99852467720240d7094d887e15f447f6930cf88
enasskmal <enasskmal@gmail.com>
2019-04-24 10:14:29 +0300

    adding a fun getdata

--- src/router.js
+++ src/router.js
@@ -38,17 +38,34 @@ const router = ((request, response) => {
 };
 });
 
-const getData =((request, response) =>{
+const getData = (request, response) => {
+  const myUrl = url.parse(request.url);
+  console.log(myUrl);
+  const query = myUrl.query;
+  console.log(query);
+  const value = querystring.parse(query);
+  console.log(value);
+  const ourUrl = `https://newsapi.org/v2/top-headlines?country=${value.url}&apiKey=05c75aad309f4dc5b04f2638474ce2cd`;
 
-const myUrl = "https://newsapi.org/v2/top-headlines?country=us&apiKey=05c75aad309f4dc5b04f2638474ce2cd";
-const { query } = myUrl.parse(request.url);
-console.log(query);
-});
-// const getData =((request, response) =>{
-//   const myUrl = "https://newsapi.org/v2/top-headlines?country=us&apiKey=05c75aad309f4dc5b04f2638474ce2cd";
-//   console.log('this is url', url.parse(myUrl));
+}//   parse the response body and put
+//   request(ourUrl, (err. response, body) => {
+//       const parsedBody = JSON.parse(body);
+//       console.log(parsedBody.url);
+//       if (err) {
+//      response.writeHead(404, {'Content-Type' : 'text/html'});
+//      response.end('Sorry, there is a server error');
+//    } else {
+//      console.log(parsedBody.rates.ILS);
+//      res.writeHead(200, {'Content-Type' : 'text/html'});
+//      res.end(parsedBody.rates.ILS);
+//    }
+//  });
 //
-// });
+// }
+
+
+
+
 module.exports = {
   router,
   getData
