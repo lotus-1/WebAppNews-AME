db7c1e283ff42dfd6431154d90f2d3e66a26f8ba
aman halabyar <amanhalabyar7@gmail.com>
2019-04-24 10:12:15 +0300

    edit the handler and the router file

--- src/router.js
+++ src/router.js
@@ -1,55 +1,15 @@
-const fs = require("fs");
-const path = require("path");
-const url = require('url');
-const querystring = require('querystring');
-const request = require('request');
+const { handlerHome, handlerPublic } = require('./handler');
 
-const router = ((request, response) => {
-  const url = request.url;
-  if (url === "/") {
-    const filePath = path.join(__dirname, "..", "public", "index.html");
-    fs.readFile(filePath, (error, file) => {
-      if (error) {
-        response.writeHead(404, { 'Content-Type': 'text/html' });
-        response.end('404 - FILE NOT FOUND');
-      } else {
-        response.writeHead(200, { 'Content-Type': 'text/html' });
-        response.end(file);
-      }
-    });
-  }else if (url.indexOf('/public/') !== -1) {
-  const extension = url.split(".")[1];
-    const extensionTypes = {
-      html : 'text/html',
-      js : 'application/javascript',
-      css: 'text/css'
-    };
-  const filePath = path.join(__dirname, '..',"public", url);
-  fs.readFile(filePath, (error, file) => {
-    if (error) {
-      console.log(error);
-      response.writeHead(404, { 'Content-Type' : 'text/html' });
-      response.end('<h1> So sorry, I can\'t find this file...</h1>');
-    } else {
-      response.writeHead(200, { 'Content-Type' : extensionTypes[extension]});
-      response.end(file);
-    }
-  });
-};
-});
+const router = (request, response) => {
+const endpoint = request.url;
 
-const getData =((request, response) =>{
-
-const myUrl = "https://newsapi.org/v2/top-headlines?country=us&apiKey=05c75aad309f4dc5b04f2638474ce2cd";
-const { query } = myUrl.parse(request.url);
-console.log(query);
-});
-// const getData =((request, response) =>{
-//   const myUrl = "https://newsapi.org/v2/top-headlines?country=us&apiKey=05c75aad309f4dc5b04f2638474ce2cd";
-//   console.log('this is url', url.parse(myUrl));
-//
-// });
-module.exports = {
-  router,
-  getData
+if (endpoint === "/") {
+  handlerHome(request, response);
+} else {
+  handlerPublic(request, response);
 }
+};
+
+
+
+module.exports = router;
