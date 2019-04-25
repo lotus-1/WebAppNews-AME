f50495de624a930915f93d04f2af0867d4dc9616
maha276 <mahaforo276@gmail.com>
2019-04-25 08:32:03 +0300

    add assets folder

--- src/server.js
+++ src/server.js
@@ -1,15 +1,6 @@
-// const http = require('http');
-// const hostname = process.env.HOSTNAME || 'localhost';
-// const port = process.env.PORT || 4000;
-//
-// http.createServer().listen(port, hostname, () => {
-//   console.log(`Server running at port http://${hostname}:${port}`)
-// });
-//
-
-const http = require('http');
-const router = require('./router');
+const http = require("http");
+const router = require("./router");
 const port = 4000;
 const server = http.createServer(router);
 server.listen(port);
-console.log('server is working in your port '+port);
+console.log("server is working in your port " + port);
